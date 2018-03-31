958321756b44b12234aea5bfa4a82151147c2bb4
lijiaman <lijiaman@buaa.edu.cn>
2018-03-31 17:41:14 -0400

    Fix issues in get_resukts_for_benchmark

--- get_results_for_benchmark.py
+++ get_results_for_benchmark.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import zipfile
 import shutil
 import h5py
+from scipy.misc import imsave
 
 import torch
 import torchvision.transforms as transforms
@@ -22,6 +23,16 @@ from prep_dataset.prep_SBD_dataset import RGB2BGR
 from prep_dataset.prep_SBD_dataset import ToTorchFormatTensor
 
 import utils.utils as utils
+
+def shifting(bitlist):
+    """
+    From https://stackoverflow.com/questions/12461361/bits-list-to-integer-in-python
+    Convert a binary list to int
+    """
+    out = 0
+    for bit in bitlist:
+        out = (out << 1) | bit
+    return out
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(sys.argv[0])
@@ -69,6 +80,7 @@ if __name__ == "__main__":
     normalize = transforms.Normalize(mean=[104.008, 116.669, 122.675], std=[1, 1, 1])
     
     img_transform = transforms.Compose([
+                    transforms.Resize([352, 352]),
                     RGB2BGR(roll=True),
                     ToTorchFormatTensor(div=False),
                     normalize,
@@ -78,11 +90,21 @@ if __name__ == "__main__":
         img = Image.open(test_lst[idx_img]).convert('RGB')
         processed_img = img_transform(img).unsqueeze(0)
         processed_img = utils.check_gpu(0, processed_img)    
-        score_feats1, score_feats2, score_feats3, score_feats5, score_fuse_feats = model(processed_img, for_vis=True)
+        score_feats5, score_fuse_feats = model(processed_img) # 1 X 20 X 352 X 352
         
+        score_output = F.sigmoid(score_fuse_feats.transpose(1,3).transpose(1,2)).squeeze(0) # H X W X 20
+        score_pred_flag = (score_output>0.5)
+        # Convert binary prediction to uint32
+        im_arr = np.empty((score_fuse_feats.size()[2], score_fuse_feats.size()[3]), np.uint32)
+        for i in xrange(score_fuse_feats.size()[2]):
+            for j in xrange(score_fuse_feats.size()[3]):
+                print("ori_binary list:{0}".format(list(score_pred_flag[i, j, :].data.cpu().numpy())))
+                im_arr[i, j] = np.uint32(shifting(list(score_pred_flag[i, j].data.cpu().numpy().astype(np.int))))
+                print("value:{0}".format(np.uint32(shifting(list(score_pred_flag[i, j].squeeze(0).data.cpu().numpy().astype(np.int))))))
+                print("binary value:{0}".format(bin(np.uint32(shifting(list(score_pred_flag[i, j].squeeze(0).data.cpu().numpy().astype(np.int)))))))
+        # Store value into img
         img_base_name_noext = os.path.splitext(os.path.basename(test_lst[idx_img]))[0]
-       
-        im = Image.fromarray() 
+        imsave(os.path.join(args.output_dir, img_base_name_noext+'.bmp'), im_arr)
         print 'processed: '+test_lst[idx_img]
     
     print 'Done!'
