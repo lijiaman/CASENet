a0fc381a0d2c443db7d1ca0c227299e3d9e52a39
Jiaman <lijiaman@users.noreply.github.com>
2018-04-07 14:47:59 -0400

    Delete run_train_sbd.sh

--- run_train_sbd.sh
+++
@@ -1,9 +0,0 @@
-srun --gres=gpu:2 -c 8 -w dgx1 -p gpuc python main.py \
---multigpu \
---lr=1e-7 \
---batch-size=10 \
---workers=1 \
---epochs=40000 \
---lr-steps 10000 20000 30000 \
---pretrained-model="./official_models/Init_CASENet.pth.tar" \
-2>&1|tee train_b10_lr1e-7.log
